d65c3babe172386a4f11dd7f9d308703165220f8
brandoncintron <bcintron19fba@gmail.com>
2025-06-28 14:23:46 -0400

    Update API rewrites for development environment

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
-      },
-    ];
+    return process.env.NODE_ENV === "development"
+      ? [
+          {
+            source: "/api/:path*",
+            destination: "http://127.0.0.1:8000/api/:path*",
+          },
+        ]
+      : [];
   },
 };
 
