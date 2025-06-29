7046b3199b8f162d4613841e6b649f210edca084
brandoncintron <bcintron19fba@gmail.com>
2025-06-28 20:45:57 -0400

    Modified next config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
       ? [
           {
             source: "/api/:path*",
-            destination: "http://127.0.0.1:8000/api/:path*",
+            destination:
+              process.env.NODE_ENV === "development"
+                ? "http://127.0.0.1:5328/api/:path*"
+                : "/api/",
           },
         ]
       : [];
