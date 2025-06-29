b7b35ab84b603fc8fa8500561fa05891ec9e11c1
brandoncintron <bcintron19fba@gmail.com>
2025-06-28 21:10:54 -0400

    Test endpoint - deleting later

--- api/pythontest.py
+++ api/pythontest.py
@@ -1,9 +1,36 @@
-from http.server import BaseHTTPRequestHandler
+from dotenv import load_dotenv
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello from pythontest!'.encode('utf-8'))
-        return
+load_dotenv()
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from api.src.chains import get_router_chain, get_tdd_chain
+from typing import List, Dict
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+if os.environ.get("VERCEL_URL"):
+    origins.append(f"https://{os.environ.get('VERCEL_URL')}")
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=origins,
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello Worldpythontest"}
+
+@app.get("/api/pythontest")
+def hello_world():
+    return {"message": "Hello World123test"}
