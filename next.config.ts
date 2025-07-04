de78ea78cdca8330524c2aebceb10bd475e7c9a3
brandoncintron <bcintron19fba@gmail.com>
2025-07-04 13:57:23 -0400

    Adjusted next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return process.env.NODE_ENV === "development"
-      ? [
-          {
-            source: "/api/:path*",
-            destination:
-              process.env.NODE_ENV === "development"
-                ? "http://127.0.0.1:8000/api/:path*"
-                : "/api/",
-          },
-        ]
-      : [];
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+    ];
   },
 };
 
