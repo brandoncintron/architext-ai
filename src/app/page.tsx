b279a8d33b4e2dfcc5989f6e79cd7a39e39c2f97
brandoncintron <bcintron19fba@gmail.com>
2025-07-09 20:39:12 -0400

    Refactored HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,19 @@
 /**
  * @file This file is the main entry point for the application's home page.
  */
-import { Github } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/mode-toggle";
+import { SiteActions } from "@/components/ui/site-actions";
 import { Wizard } from "@/components/wizard/wizard";
 
 const HomePage = () => {
   return (
     <>
-      <Wizard />
-      {/* 
-      <div className="absolute right-8 top-6 flex items-center space-x-8">
-        <Button
-          variant="outline"
-          size="icon"
-          className="animate-pulse-scale rounded-full h-12 w-12"
-          asChild
-        >
-          <a
-            href="https://github.com/brandoncintron/architext-ai"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github className="h-5 w-5" />
-            <span className="sr-only">GitHub</span>
-          </a>
-        </Button>
-        <ModeToggle />
-      </div>*/}
+      <header className="flex w-full items-center justify-end pr-4 pt-4">
+        <SiteActions />
+      </header>
+
+      <div className="flex flex-grow items-center justify-center">
+        <Wizard />
+      </div>
     </>
   );
 };
