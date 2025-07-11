5507e80c4036c756cdc5a5d9c38bc56c051f1da7
brandoncintron <bcintron19fba@gmail.com>
2025-07-11 11:43:02 -0400

    Added support for mermaid code

--- src/components/wizard/results/results-step.tsx
+++ src/components/wizard/results/results-step.tsx
@@ -3,11 +3,20 @@
  */
 "use client";
 
-import React, { useState } from "react";
+import React, {
+  Fragment,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { getCodeString } from "rehype-rewrite";
 
 import { EditorView } from "@codemirror/view";
 import { Check, Clipboard, Eye, Pencil, RotateCcw } from "lucide-react";
+import mermaid from "mermaid";
 import dynamic from "next/dynamic";
+import rehypeSanitize from "rehype-sanitize";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -32,11 +41,67 @@ const MarkdownPreview = dynamic(
   { ssr: false, loading: () => <p>Loading preview...</p> },
 );
 
+const rehypePlugins = [rehypeSanitize];
+
 const largeTextTheme = EditorView.theme({
   "&": {
     fontSize: "1rem",
   },
 });
+
+const randomid = () => parseInt(String(Math.random() * 1e15), 10).toString(36);
+
+const Code = ({
+  inline,
+  children = [],
+  className,
+  ...props
+}: {
+  inline?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+  node?: any;
+}) => {
+  const demoid = useRef(`dome${randomid()}`);
+  const [container, setContainer] = useState<HTMLElement | null>(null);
+  const isMermaid =
+    className && /^language-mermaid/.test(className.toLocaleLowerCase());
+  const code =
+    props.node && props.node.children
+      ? getCodeString(props.node.children)
+      : React.Children.toArray(children)[0] || "";
+
+  const reRender = async () => {
+    if (container && isMermaid) {
+      try {
+        const { svg } = await mermaid.render(demoid.current, code as string);
+        container.innerHTML = svg;
+      } catch (error: any) {
+        container.innerHTML = error.toString();
+      }
+    }
+  };
+
+  useEffect(() => {
+    reRender();
+  }, [container, isMermaid, code, demoid]);
+
+  const refElement = useCallback((node: HTMLElement | null) => {
+    if (node !== null) {
+      setContainer(node);
+    }
+  }, []);
+
+  if (isMermaid) {
+    return (
+      <Fragment>
+        <code id={demoid.current} style={{ display: "none" }} />
+        <code ref={refElement} data-name="mermaid" />
+      </Fragment>
+    );
+  }
+  return <code>{children}</code>;
+};
 
 export const ResultsStep = ({ tdd, onStartOver }: ResultsStepProps) => {
   const [markdown, setMarkdown] = useState(tdd);
@@ -50,6 +115,8 @@ export const ResultsStep = ({ tdd, onStartOver }: ResultsStepProps) => {
         <MarkdownPreview
           source={markdown}
           className="h-full overflow-y-auto p-8"
+          components={{ code: Code }}
+          rehypePlugins={rehypePlugins}
         />
       ) : (
         <MarkdownEditor
@@ -82,6 +149,8 @@ export const ResultsStep = ({ tdd, onStartOver }: ResultsStepProps) => {
         <MarkdownPreview
           source={markdown}
           className="h-full overflow-y-auto p-8"
+          components={{ code: Code }}
+          rehypePlugins={rehypePlugins}
         />
       </ResizablePanel>
     </ResizablePanelGroup>
@@ -102,7 +171,7 @@ export const ResultsStep = ({ tdd, onStartOver }: ResultsStepProps) => {
           </div>
           <div className="flex w-full flex-wrap items-center justify-center gap-2 space-x-2 md:w-auto md:flex-shrink-0 md:justify-start">
             <Button onClick={onStartOver}>
-            <RotateCcw className="mr-2 h-4 w-4" />
+              <RotateCcw className="mr-2 h-4 w-4" />
               Start Over
             </Button>
             <Button
