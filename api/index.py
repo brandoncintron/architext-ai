8097859b6cbd71a0fa178deab06b10c6c632db2b
brandoncintron <bcintron19fba@gmail.com>
2025-07-11 11:40:53 -0400

    Removed old vercel reference

--- api/index.py
+++ api/index.py
@@ -4,7 +4,6 @@ load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import os
 
 from api.generate_plan import router as plan_router
 from api.generate_tdd import router as tdd_router
@@ -14,11 +13,6 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
 ]
-
-# Add Vercel deployment URL
-if os.environ.get("VERCEL_URL"):
-    origins.append(f"https://{os.environ.get('VERCEL_URL')}")
-
 
 app.add_middleware(
     CORSMiddleware,
